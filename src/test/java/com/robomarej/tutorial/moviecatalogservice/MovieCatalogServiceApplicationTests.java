c89edd1bd3ecc43dc867b4a238adfcec291d4ce0
robo <robomarej@gmail.com>
2020-02-05 23:42:36 +0100

    Initial commit

---
+++ src/test/java/com/robomarej/tutorial/moviecatalogservice/MovieCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robomarej.tutorial.moviecatalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
