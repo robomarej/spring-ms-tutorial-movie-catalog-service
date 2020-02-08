61be838eb7e60e0f9f8075e33df6536aaa99e7fe
robo <robomarej@gmail.com>
2020-02-08 20:14:25 +0100

    circuit breaker via timeout

--- src/main/java/com/robomarej/tutorial/moviecatalogservice/MovieCatalogServiceApplication.java
+++ src/main/java/com/robomarej/tutorial/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -2,21 +2,29 @@ package com.robomarej.tutorial.moviecatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
 
 @SpringBootApplication
 public class MovieCatalogServiceApplication {
 
     @Bean
+    @Primary
     @LoadBalanced
-    public RestTemplate getRestTemplate() {
-		return new RestTemplate();
-	}
+    public RestTemplate getRestTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder
+                .setReadTimeout(Duration.ofSeconds(5))
+                .setConnectTimeout(Duration.ofSeconds(5))
+                .build();
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(MovieCatalogServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MovieCatalogServiceApplication.class, args);
+    }
 
 }
