eb54a9469f768262ee29bf9c4596f3a47a6f8af1
robo <robomarej@gmail.com>
2020-02-09 17:47:30 +0100

    hystrix stream/monitoring

--- src/main/java/com/robomarej/tutorial/moviecatalogservice/MovieCatalogServiceApplication.java
+++ src/main/java/com/robomarej/tutorial/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 public class MovieCatalogServiceApplication {
 
     @Bean
